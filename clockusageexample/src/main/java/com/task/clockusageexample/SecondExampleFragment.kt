89a5e961e697fb856f8e85cb47d51e1b7931cb94
Kirill Mokhnatkin <librarian4832@gmail.com>
2023-03-04 00:00:04 +0300

    Made pushing button on the second screen change the color of the second hand of the clock

--- clockusageexample/src/main/java/com/task/clockusageexample/SecondExampleFragment.kt
+++ clockusageexample/src/main/java/com/task/clockusageexample/SecondExampleFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import com.task.clockcustomview.ClockView
 
 class SecondExampleFragment : Fragment() {
 
@@ -13,6 +16,18 @@ class SecondExampleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second_example, container, false)
+        val view = inflater.inflate(R.layout.fragment_second_example, container, false)
+        // Clicking on the button makes the second hand change color
+        // You can check that view instance state is saved by changing the second hand color,
+        // rotating the screen and seeing that the color you picked was saved
+        val button = view.findViewById<Button>(R.id.run_oks_button)
+        button.setOnClickListener {
+            val clock = view.findViewById<ClockView>(R.id.clockView_oks)
+            if (clock.secondHandColor == ContextCompat.getColor(view.context, R.color.black))
+                clock.secondHandColor = ContextCompat.getColor(view.context, R.color.bright_red)
+            else
+                clock.secondHandColor = ContextCompat.getColor(view.context, R.color.black)
+        }
+        return view
     }
 }
